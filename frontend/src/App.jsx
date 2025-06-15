c9badf234d05fedcb9de76b32d184bec26d83529
Areeba Fatima Ansari <130861170+areeba-bytesmith@users.noreply.github.com>
2025-06-15 17:46:35 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PredictionForm from './Component/PredictionForm.jsx';
+
+const App = () => {
+  return (
+    <>
+      <PredictionForm />
+    </>
+  );
+};
+
+export default App;
